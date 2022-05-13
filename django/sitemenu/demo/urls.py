0d17b037a1aa383e2a324d2d7bf3f46d5bfa9552
eunjinchoi-hub <83259618+eunjinchoi-hub@users.noreply.github.com>
2022-05-13 20:13:23 +0900

    web prototype

---
+++ django/sitemenu/demo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'demo'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('board/', views.board, name='board'),
+    path('home/', views.home, name='home'),
+]
