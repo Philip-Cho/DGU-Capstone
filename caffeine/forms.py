a92ec415c46ccef7e9046c61ac9fa0a938bd50e9
Philip Cho <tjdxor38@gmail.com>
2022-05-22 23:16:17 +0900

    회원가입 생성

---
+++ caffeine/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from caffeine.models import Users
+
+class RegisterForm(UserCreationForm):
+    id = forms.CharField(max_length=20, label='ID')
+    username = forms.CharField(max_length=20, label='이름')
+    email = forms.EmailField(max_length=40, label='e-mail')
+    
+    class Meta:
+        model = Users
+        fields = {
+            'username',
+            'id',
+            'email',
+            'password1',
+            'password2',
+        }
