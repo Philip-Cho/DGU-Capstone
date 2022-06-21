72c3180cc6d7cb406321fc9faa0c2ce8d42b0dd3
Shim-JS <s961215@naver.com>
2022-06-21 14:34:49 +0900

    - 한국어 STT 모듈 작성

--- caffeine/tools/stt.py
+++ caffeine/tools/stt.py
@@ -19,9 +19,9 @@ def upload_blob_from_memory(bucket_name, contents, destination_blob_name):
     )
 
 
-# STT
-def transcribe_gcs(gcs_uri, content, sample_rate_hertz):
-    print("STT 시작")
+# STT_English
+def transcribe_gcs_en(gcs_uri, content, sample_rate_hertz):
+    print("영어 STT 시작")
     ## 가중치 파일로드
     words_list = list()
     with open("text/sciwords.txt") as f:
@@ -29,6 +29,7 @@ def transcribe_gcs(gcs_uri, content, sample_rate_hertz):
         for i in text.split():
             words_list.append(i)
 
+    ## 가중치 부여
     boost = 10.0
     speech_contexts_element = {"phrases": words_list, "boost": boost}
     speech_contexts = [speech_contexts_element]
@@ -49,9 +50,6 @@ def transcribe_gcs(gcs_uri, content, sample_rate_hertz):
 
     operation = client.long_running_recognize(config=config, audio=audio)
     response = operation.result()
-    #     text =  ''
-    #     for temp in response.results:
-    #         text += temp.alternatives[0].transcript
 
     text_2 = ''
     text_1 = ''
@@ -62,16 +60,51 @@ def transcribe_gcs(gcs_uri, content, sample_rate_hertz):
         else:
             text_1 += result.alternatives[0].transcript + ' '
 
-    # # path가져오기
-    # path = os.getcwd()
-    # folder_text = "text"
-    # text_file = os.path.join(path, folder_text, content)
-    #
-    # f = open('{}.txt'.format(text_file), 'w')
-    # f.write(text_2)
-    # f.close()
-    print("STT 완료")
+    print("영어 STT 완료")
+
+    return text_2
 
 
+# STT_Korean
+def transcribe_gcs_kor(gcs_uri, content, sample_rate_hertz):
+    print("한국어 STT 시작")
+    # ## 가중치 파일로드
+    # words_list = list()
+    # with open("text/sciwords.txt") as f:
+    #     text = f.read()
+    #     for i in text.split():
+    #         words_list.append(i)
+
+    # ## 가중치 부여
+    # boost = 10.0
+    # speech_contexts_element = {"phrases": words_list, "boost": boost}
+    # speech_contexts = [speech_contexts_element]
+
+    client = speech.SpeechClient()
+    audio = speech.RecognitionAudio(uri=gcs_uri)
+    config = speech.RecognitionConfig(
+        # wav 파일이므로 Linear16, Flac 파일은 FLAC
+        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+        # hertz 16000, 44100(2개의 오디오 채널), Flac은 또 다르다.
+        sample_rate_hertz=sample_rate_hertz,  # 48000
+        language_code="ko-KR",
+        audio_channel_count=2,
+        enable_separate_recognition_per_channel=True,
+        enable_automatic_punctuation=True
+    )
+
+    operation = client.long_running_recognize(config=config, audio=audio)
+    response = operation.result()
+
+    text_2 = ''
+    text_1 = ''
+
+    for result in response.results:
+        if result.channel_tag == 2:
+            text_2 += result.alternatives[0].transcript + ' '
+        else:
+            text_1 += result.alternatives[0].transcript + ' '
+
+    print("한국어 STT 완료")
 
     return text_2
